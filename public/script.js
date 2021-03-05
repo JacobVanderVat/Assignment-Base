6187e0e7026233784b8003037ac8dd855c7337e0
JacobVanderVat <jacobvandervat@gmail.com>
2021-03-05 12:52:04 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -12,21 +12,20 @@ async function windowActions() {
   body: JSON.stringify({data: search.value})
   });
   const data = await request.json();
-
+  //console.table(data);
   form.addEventListener("keyup", async (event) => {
     event.preventDefault();
     console.log("submit fired", search.value);
-    const filtered = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase);
+    const filtered = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase());
     filtered.forEach((item) => {
         const appendItem = document.createElement("li");
-        appendItem.innerText = item.name;
+        appendItem.innerText = item.city;
         targetList.append(appendItem);
     });
   });
 
   search.addEventListener('input', (event) => {
     console.log('input', event.target.value);
-
   });
 
 
