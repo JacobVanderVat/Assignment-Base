bfe31720634c8aa3eafe727f7b4b877907202ce0
JacobVanderVat <jacobvandervat@gmail.com>
2021-03-05 02:04:24 -0500

    Changed to match lecture

--- public/script.js
+++ public/script.js
@@ -1,64 +1,34 @@
 async function windowActions() {
-  const search = document.querySelector(".input");
+  console.log("Window Loaded");
   const form = document.querySelector(".userform");
+  const search = document.querySelector("#Zipcode");
+  const targetList = document.querySelector('target-list');
 
   const request = await fetch('/api', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-  
   body: JSON.stringify({data: search.value})
   });
-  //const data = await request.json();
+  const data = await request.json();
 
   form.addEventListener("keyup", async (event) => {
     event.preventDefault();
-    console.log("submit fired");
-    const food = [];
-    displayMatches();
+    console.log("submit fired", search.value);
+    const filtered = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase);
+    filtered.forEach((item) => {
+        const appendItem = document.createElement("li");
+        appendItem.innerText = item.name;
+        targetList.append(appendItem);
+    });
+  });
 
-    function findMatches(wordToMatch, food) {
-      return food.filter((place) => {
-        const regex = new RegExp(wordToMatch, "gi");
-        return place.zip.match(regex);
-      });
-    }
-    function displayMatches() {
-      const matchArray = findMatches(this.value, food);
-      const html = matchArray
-        .map((place) => {
-          const regex = new RegExp(this.value, "gi");
-          const zipper = place.zip.replace(
-            regex,
-            `<span class = h1>${this.value}</span>`
-          );
-          return `
-        <li>
-            <span class="name">${zipper}</span>
-            <span class="category">${place.category}</span>
-            <span class="address_line_1">${place.address_line_1}</span>
-            <span class="city">${place.city}</span>
-            <span class="zip">${place.zip}</span>
-        </li>
-        `;
-        })
-        .join("");
-      suggestions.innerHTML = html;
-    } 
-});
-   
-    
-  
+  search.addEventListener('input', (event) => {
+    console.log('input', event.target.value);
+
+  });
+
+
 }
-
 window.onload = windowActions;
-
-
-   // const request = await fetch("/api", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({ data: searchInput.value }),
-    // });
